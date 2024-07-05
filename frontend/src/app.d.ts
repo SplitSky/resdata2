83fb10a0eb629f2600c78e68abd4b7a425b23370
SplitSky <opop.neska60@gmail.com>
2024-07-05 17:50:08 +0100

    Added the components and set up config files properly. Still need ot add tailwind

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,22 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-//declare global {
-//	namespace App {
-//		// interface Error {}
-//		// interface Locals {}
-//		// interface PageData {}
-//		// interface PageState {}
-//		// interface Platform {}
-//	}
-//}
-//
-//export {};
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
 
-/// importing tailwind into main
-import './styles/tailwind.css';
-import App from './App.svelte';
-const app = new App({
-	target : document.body,
-});
-
-export default app;
+export {};
--- frontend/src/pages/Home.svelte
+++
@@ -1,4 +0,0 @@
-<section class="container mx-auto p-4">
-    <h1 class="text-3xl font-bold">Welcome to Resdata2</h1>
-    <p class="mt-4">Resdata2 is designed to help you manage and analyze your research data efficiently...</p>
-  </section>
