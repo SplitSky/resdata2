7b52fb950521880b699d9f36f2d8687a25980cf5
SplitSky <opop.neska60@gmail.com>
2025-09-14 23:42:12 +0100

    refactor: cleaned the branch and started working on actix api from the beginning

---
+++ backend/resdata/src/main.rs
@@ -0,0 +1,15 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
